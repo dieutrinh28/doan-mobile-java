93a3ba2ff438ebfaba11f16a2543e3b70a92dcb4
hanha <hanhamho963@gmail.com>
2023-04-04 14:10:56 +0700

    change ui

--- app/src/main/java/com/example/bewithyou/Register.java
+++ app/src/main/java/com/example/bewithyou/Register.java
@@ -13,16 +13,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import java.util.HashMap;
 
 public class Register extends AppCompatActivity {
 
-    private EditText username;
     private EditText name;
-    private EditText email;
-    private EditText password;
+    private EditText email, password, username;
     private Button register;
     private TextView loginUser;
 
@@ -40,43 +43,45 @@ public class Register extends AppCompatActivity {
 //        name = findViewById(R.id.name);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
+        username = findViewById(R.id.username);
         register = findViewById(R.id.register);
 
-        Auth =FirebaseAuth.getInstance();
+        Auth = FirebaseAuth.getInstance();
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String txt_email = email.getText().toString();
                 String txt_password = password.getText().toString();
+                String txt_name = username.getText().toString();
 
                 if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
                     Toast.makeText(Register.this, "Empty credentials!", Toast.LENGTH_SHORT).show();
                 } else if (txt_password.length() < 6) {
                     Toast.makeText(Register.this, "Password too short!", Toast.LENGTH_SHORT).show();
                 } else {
-                    registerUser(txt_email, txt_password);
+                    registerUser(txt_name, txt_email, txt_password);
                 }
             }
 
 
         });
     }
-    private void registerUser(String email, String password)
-    {
-        Auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(Register.this,  new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(Register.this, "Registering user successfull", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(Register.this, MainActivity.class));
-                    finish();
-                }else {
-                    Toast.makeText(Register.this, "Registration failed!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-    }
+//    private void registerUser(String email, String password)
+//    {
+//        Auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(Register.this,  new OnCompleteListener<AuthResult>() {
+//            @Override
+//            public void onComplete(@NonNull Task<AuthResult> task) {
+//                if(task.isSuccessful()){
+//                    Toast.makeText(Register.this, "Registering user successfull", Toast.LENGTH_SHORT).show();
+//                    startActivity(new Intent(Register.this, MainActivity.class));
+//                    finish();
+//                }else {
+//                    Toast.makeText(Register.this, "Registration failed!", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+//
+//    }
 
 //        loginUser = findViewById(R.id.login_user);
 
@@ -104,46 +109,25 @@ public class Register extends AppCompatActivity {
 //        });
 //    }
 
-//    private void registerUser(final String username, final String name, final String email, String password) {
-//
-//        pd.setMessage("Please Wait!");
-//        pd.show();
-//
-//        mAuth.createUserWithEmailAndPassword(email , password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-//            @Override
-//            public void onSuccess(AuthResult authResult) {
-//
-//                HashMap<String , Object> map = new HashMap<>();
-//                map.put("name" , name);
-//                map.put("email", email);
-//                map.put("username" , username);
-//                map.put("id" , mAuth.getCurrentUser().getUid());
-//                map.put("bio" , "");
-//                map.put("imageurl" , "default");
-//
-//                mRootRef.child("Users").child(mAuth.getCurrentUser().getUid()).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<Void> task) {
-//                        if (task.isSuccessful()){
-//                            pd.dismiss();
-//                            Toast.makeText(Register.this, "Update the profile " +
-//                                    "for better expereince", Toast.LENGTH_SHORT).show();
-//                            Intent intent = new Intent(Register.this , MainActivity.class);
-//                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//                            startActivity(intent);
-//                            finish();
-//                        }
-//                    }
-//                });
-//
-//            }
-//        }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception e) {
-//                pd.dismiss();
-//                Toast.makeText(Register.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-//            }
-//        });
+    private void registerUser(final String username, final String email, String password) {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign up successful, update the user's display name with their username
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(username)
+                                    .build();
+                            user.updateProfile(profileUpdates);
 
-
+                            // TODO: Save the user's information to a database or other storage
+                        } else {
+                            // Sign up failed, display a message to the user
+                            Toast.makeText(getApplicationContext(), "Registration failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 }
