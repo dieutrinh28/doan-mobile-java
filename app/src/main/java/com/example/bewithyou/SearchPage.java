0b44821714cb34ffbc90aa8ee366551d78713bee
NganNguyenThiBich <bichngan.bnnt@gmail.com>
2023-04-10 09:34:14 +0700

    logout

--- app/src/main/java/com/example/bewithyou/SearchPage.java
+++ app/src/main/java/com/example/bewithyou/SearchPage.java
@@ -3,6 +3,7 @@ package com.example.bewithyou;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +17,9 @@ import android.widget.Toast;
 
 import com.example.bewithyou.model.Product;
 import com.example.bewithyou.model.User;
+import com.example.bewithyou.ui.auth.LoginPage;
+import com.example.bewithyou.ui.auth.RegisterPage;
+import com.example.bewithyou.ui.home.HomePage;
 
 import java.util.List;
 
@@ -31,6 +35,20 @@ public class SearchPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_page);
 
+        SharedPreferences preferences = getSharedPreferences("MyPreferences", MODE_PRIVATE);
+
+
+        button = findViewById(R.id.btnLogOut);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("username", "");
+                editor.apply();
+                startActivity(new Intent(SearchPage.this , LoginPage.class));
+            }
+        });
+
         ImageButton backButton = findViewById(R.id.back_button);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,7 +60,6 @@ public class SearchPage extends AppCompatActivity {
         phoneNum=findViewById(R.id.phone_num_text);
         address =findViewById(R.id.btnDeliveryAddress);
 
-        SharedPreferences preferences = getSharedPreferences("MyPreferences", MODE_PRIVATE);
         String username = preferences.getString("username", "default_value");
 
         getData.getUser(username, new Callback<User>() {
